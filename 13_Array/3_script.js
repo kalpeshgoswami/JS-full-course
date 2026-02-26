862a80dfd201707e98f5a83ed07e84491b3fc515
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-02-26 11:02:59 +0530

    in array script-4 add

--- 13_Array/3_script.js
+++ 13_Array/3_script.js
@@ -23,11 +23,39 @@ videoGames.pop();
 console.log("After pop", videoGames);
 
 
-
 let mobileGames = ["Free Fire", "PUBG", "Clash of Clans"];
 
 mobileGames.shift();
 
 console.log("After shift", mobileGames);
 
+
+
+
+// The splice() method
+
+
+let fruits = ["Apple", "Banana", "Orange", "Mango"];
+
+console.log("Original fruits:", fruits);
+
+
+let removedFruits = fruits.splice(1, 2);
+
+console.log("Removed fruits:", removedFruits);
+console.log("Fruits after removal:", fruits);
+
+
+let numbers = [1, 2, 5];
+console.log("Original numbers:", numbers);
+
+numbers.splice(2, 0, 3, 4);
+console.log("Numbers after adding elements:", numbers);
+
+let colors = ["Red", "Blue", "Green"];
+console.log("Original colors:", colors);
+
+let updateColor = colors.splice(1, 1, "Yellow");
+console.log("color:", updateColor);
+console.log("Colors after update:", colors);
 
---
+++ 13_Array/4_script.js
@@ -0,0 +1,28 @@
+
+
+
+let numbers = [1, 2, 3, 4];
+
+console.log("Using forEach():");
+
+numbers.forEach(function(number) {
+    console.log("Number:", number);
+});
+
+let array1 = ["Apple", "Banana"];
+let array2 = ["Orange", "Mango"];
+
+let combinedArray = array1.concat(array2);
+
+console.log("Array 1:", array1);
+console.log("Array 2:", array2);
+console.log("Combined Array:", combinedArray);
+
+
+let colors = ["Red", "Green", "Blue"];
+
+
+let colorsString = colors.toString();
+
+console.log("Original Array:", colors);
+console.log("Converted to String:", colorsString);
