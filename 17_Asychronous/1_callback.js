9c9f58b332e6d3e386357a131f99f093aad6d384
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-03-22 20:42:06 +0530

    add callback defination

--- 17_Asychronous/1_callback.js
+++ 17_Asychronous/1_callback.js
@@ -1,4 +1,11 @@
+// Where callbacks really shine are in asynchronous functions, where one function has to wait for another function (like waiting for a file to load).
 
+// if you want to wait for the result of the previous function call before the next statement is executed, you can use a callback function.
+
+// callback is function passed as an argument to another function
+// using callback we can do asynchronous task in js
+// a callback function it runs after another function has finished
+// when you pass functions as an arguments don't use parenthesis
 
 function total (result) {
     console.log(result);
