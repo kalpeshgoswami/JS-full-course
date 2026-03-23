40d1a684b412daa70fecbd9a7b3e1bab9efe22bc
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-03-23 15:20:14 +0530

    add new example

--- 17_Asychronous/6_callbackHell.js
+++ 17_Asychronous/6_callbackHell.js
@@ -1,37 +1,93 @@
-function wakeUp(callback) {
-  setTimeout(() => {
-    console.log("Woke up");
-    callback();
-  }, 1000);
+// // ----- Example 1 -----
+
+// function wakeUp(callback) {
+//   setTimeout(() => {
+//     console.log("Woke up");
+//     callback();
+//   }, 1000);
+// }
+
+// function brushTeeth(callback) {
+//   setTimeout(() => {
+//     console.log("Brushed teeth");
+//     callback();
+//   }, 1000);
+// }
+
+// function haveBreakfast(callback) {
+//   setTimeout(() => {
+//     console.log("Had breakfast");
+//     callback();
+//   }, 1000);
+// }
+
+// function goToWork(callback) {
+//   setTimeout(() => {
+//     console.log("Went to work");
+//     callback();
+//   }, 1000);
+// }
+
+// wakeUp(() => {
+//   brushTeeth(() => {
+//     haveBreakfast(() => {
+//       goToWork(() => {
+//         console.log("Day started!");
+//       });
+//     });
+//   });
+// });
+
+
+
+//  -----Example 2 -----
+
+function startGame(callback){
+    setTimeout(()=>{
+        console.log("Game started")
+        callback();
+    },1000)
 }
 
-function brushTeeth(callback) {
-  setTimeout(() => {
-    console.log("Brushed teeth");
-    callback();
-  }, 1000);
+function loadLevel(callback){
+    setTimeout(()=>{
+        console.log("Level loaded")
+        callback();
+    },1000)
 }
 
-function haveBreakfast(callback) {
-  setTimeout(() => {
-    console.log("Had breakfast");
-    callback();
-  }, 1000);
+function spawnPlayer(callback){
+    setTimeout(()=>{
+        console.log("Player spawned")
+        callback();
+    },1000)
 }
 
-function goToWork(callback) {
-  setTimeout(() => {
-    console.log("Went to work");
-    callback();
-  }, 1000);
+function fightEnemies(callback){
+    setTimeout(()=>{
+        console.log("Enemies defeated")
+        callback();
+    },1000)
 }
 
-wakeUp(() => {
-  brushTeeth(() => {
-    haveBreakfast(() => {
-      goToWork(() => {
-        console.log("Day started!");
-      });
-    });
-  });
-});
+function completeMission(callback){
+    setTimeout(()=>{
+        console.log("Mission completed")
+        callback();
+    },1000)
+}
+
+startGame(()=>{
+    loadLevel(()=>{
+        spawnPlayer(()=>{
+            fightEnemies(()=>{
+                completeMission(()=>{
+                    setTimeout(()=>{
+                        console.log("Level Complete! Rewards unlocked")
+                    },2000)
+                })
+            })
+        })
+    })
+})
+
