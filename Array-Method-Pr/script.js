94ef19d857f5de491d56c0633fa754103702027e
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-04-28 13:28:26 +0530

    add css

--- Array-Method-Pr/script.js
+++ Array-Method-Pr/script.js
@@ -52,3 +52,177 @@ function pushBtn() {
     document.getElementById("inputValue").value = "";
 }
 
+function unshiftBtn() {
+    let value = document.getElementById("inputValue").value
+
+    if (value !== "") {
+        arr.unshift(value)
+        updateArr()
+    }
+
+    document.getElementById("inputValue").value = ""
+}
+
+function popBtn() {
+    if (arr.length === 0) loadArray()
+
+    arr.pop()
+    updateArr()
+}
+
+function shiftBtn() {
+    if (arr.length === 0) loadArray()
+
+    arr.shift()
+    updateArr()
+}
+
+function reverseBtn() {
+    if (arr.length === 0) loadArray()
+
+    arr.reverse()
+    updateArr()
+}
+
+function toStringBtn() {
+    if (arr.length === 0) loadArray();
+
+    Result .innerHTML = "Output: " + arr.toString();
+};
+
+function lengthBtn() {
+    if (arr.length === 0) {
+        Result.innerHTML = "Array is empty!";
+        return;
+    }
+
+    Result .innerHTML = "Length: " + arr.length;
+}
+
+function indexOfBtn(){
+    let value = document.getElementById("inputValue").value;
+
+    if (arr.length === 0) loadArray();
+
+    let index = arr.indexOf(value);
+
+    Result .innerHTML = "Index of " + value + " : " + index;
+
+    document.getElementById("inputValue").value = "";
+}
+
+
+function includesBtn(){
+    let value = document.getElementById("inputValue").value;
+
+    if (arr.length === 0) loadArray();
+
+    let ans = arr.includes(value);
+
+    Result .innerHTML = "Includes " + value + " : " + ans;
+
+    document.getElementById("inputValue").value = "";
+}
+
+function sortBtn(){
+    if (arr.length === 0) loadArray();
+
+    arr.sort((a, b) => a - b);
+    updateArr();
+}
+
+function ascendingBtn() {
+    if (arr.length === 0) loadArray();
+
+    arr.sort((a, b) => a - b);
+    updateArr();
+};
+
+function descendingBtn() {
+    if (arr.length === 0) loadArray();
+
+    arr.sort((a, b) => b - a);
+    updateArr();
+};
+
+function findBtn(){
+    let value = Number(document.getElementById("inputValue").value);
+
+    if (arr.length === 0) loadArray();
+
+    let result = arr.find(el => el > value);
+
+    Result .innerHTML = "First element greater than " + value + " : " + result;
+
+    document.getElementById("inputValue").value = "";
+}
+
+function someBtn(){
+    let value = Number(document.getElementById("inputValue").value);
+
+    if (arr.length === 0) loadArray();
+
+    let result = arr.some(el => el > value);
+
+    Result .innerHTML = "Any element greater than " + value + " : " + result;
+
+    document.getElementById("inputValue").value = "";
+}
+
+function everyBtn(){
+    let value = Number(document.getElementById("inputValue").value);
+
+    if (arr.length === 0) loadArray();
+
+    let result = arr.every(el => el > value);
+
+    Result .innerHTML = "All elements greater than " + value + " : " + result;
+
+    document.getElementById("inputValue").value = "";
+}
+
+function filterBtn(){
+    let value = Number(document.getElementById("inputValue").value);
+
+    if (arr.length === 0) loadArray();
+
+    let result = arr.filter(el => el %2===0);
+
+  
+        Result .innerHTML = "Even  Number : [ " + result.join(", ") + " ]";
+    
+
+    document.getElementById("inputValue").value = "";
+}
+
+function mapBtn(){
+    if (arr.length === 0) loadArray();
+
+    let result = arr.map(el => el * 2);
+
+    Result .innerHTML = "multiplication *2 : [ " + result.join(", ") + " ]";
+}
+
+function reduceBtn(){
+    if (arr.length === 0) loadArray();
+
+    let result = arr.reduce((total, el) => total + el, 0);
+
+    Result .innerHTML = "Sum of Array: " + result
+}
+
+function sliceBtn(){
+    if (arr.length === 0) loadArray()
+
+    let result = arr.slice(1, 4)
+
+    Result .innerHTML = "Slice : [ " + result.join(", ") + " ]"
+}
+
+function joinBtn(){
+    if (arr.length === 0) loadArray();
+
+    let result = arr.join(" - ");
+
+    Result .innerHTML = "Joined: " + result;
+}
